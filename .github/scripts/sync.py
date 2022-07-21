e6ab0e1b885201eaf4bfaecc48ee3c936db719fc
PranavNair0001 <pranavnair@google.com>
2022-07-21 12:13:16 +0000

    Extract GCS Bucket List, Increase Retry Timeout

--- .github/scripts/sync.py
+++ .github/scripts/sync.py
@@ -13,9 +13,29 @@
 # the License.
 
 import utilities
+import os
+import logging
+import json
+
+logging.getLogger().setLevel(logging.DEBUG)    # Enable logging in GitHub Workflow
+
+central_bucket, regional_buckets = utilities.get_bucket_list()
+
+jsonStr = ''
+for bucket in regional_buckets:
+    jsonStr += '{\"central_address\":\"%s\",\"regional_address\":\"%s\"},' %('gs://' + central_bucket, 'gs://' + bucket + '/hub')
+
+output = '[' + jsonStr[:-1] + ']'
+logging.debug('Output of list.py: ')
+logging.debug(json.dumps(json.loads(output), indent=2))    # Pretty print JSON output
+
+env_file = os.getenv('GITHUB_ENV')
+with open(env_file, "a") as myfile:
+    myfile.write("buckets=" + str(output))
+
 # commands to sync the local repo in the runner to GCS central production bucket
 # -c flag : to compute and compare checksums (instead of comparing mtime) for files
 
-utilities.run_shell_command('gsutil -m rsync -d -c -r -n packages/ gs://${CENTRAL_GCS_BUCKET_PREFIX}/packages/')
-utilities.run_shell_command('gsutil cp -n categories.json gs://${CENTRAL_GCS_BUCKET_PREFIX}/categories.json')
-utilities.run_shell_command('gsutil cp -n packages.json gs://${CENTRAL_GCS_BUCKET_PREFIX}/packages.json')
+utilities.run_shell_command('gsutil -m rsync -d -c -r -n packages/ gs://' + central_bucket + '/packages/')
+utilities.run_shell_command('gsutil cp -n categories.json gs://' + central_bucket + '/categories.json')
+utilities.run_shell_command('gsutil cp -n packages.json gs://' + central_bucket + '/packages.json')
